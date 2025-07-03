575f6f8b3282e55f253caee3bbbd37d1207b286b
Mirza <mirza.suba@gmail.com>
2025-07-03 16:03:27 +0200

    members btns

--- src/app/groups/[groupId]/members/page.tsx
+++ src/app/groups/[groupId]/members/page.tsx
@@ -1,5 +1,6 @@
 import colors from "@/components/colors";
 import MapMembers from "@/components/MapMembers";
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 export default function GroupMembersPage() {
@@ -27,6 +28,16 @@ export default function GroupMembersPage() {
       <h2 className="text-xl font-semibold">Members</h2>
       <div className="bg-muted/50 rounded-lg p-8 text-center">
         <MapMembers users={members} />
+
+        <div className="w-full flex flex-col items-center justify-center gap-5 mt-5 md:flex-row">
+          <Button className="flex-1 w-full" size="xl">
+            Add Members
+          </Button>
+
+          <Button className="flex-1 w-full" size="xl" variant="destructive">
+            Kick Members
+          </Button>
+        </div>
       </div>
     </div>
   );
